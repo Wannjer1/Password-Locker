d908c53f0bfc47cd0b903f30a2e64f25eb4d75d7
Ann Wanjeri <wannjeri2@gmail.com>
2022-04-26 13:27:21 +0300

    created contact object

--- user_test.py
+++ user_test.py
@@ -1,2 +1,11 @@
 import unittest
 from user import User
+
+class Testuser (unittest.TestCase):
+    """Test class that defines test cases for the user class behavours
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases"""
+
+    def setUp(self):
+        '''set up method to run before each test case'''
+        self.new_user = User("Ann", "annie@gmail.com", "#pass123")
